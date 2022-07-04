5330d5f0df2a433e266219003e7e50f30ea8a614
Yaël Guilloux <yael.guilloux@gmail.com>
2022-07-04 17:42:06 +0200

    fix(nuxt.config): revert config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,17 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  extends: ['@nuxt-theme/alpine']
+  modules: ['nuxt-theme-kit/module', '@nuxtjs/tailwindcss', '@nuxt/content'],
+  content: {
+    documentDriven: true
+  },
+  theme: {
+    meta: {
+      name: 'Alpine',
+      description: 'Just a basic blog theme for Nuxt.',
+      author: 'NuxtLabs'
+    },
+    options: true,
+    tokens: true
+  }
 })
