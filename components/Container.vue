004c70a9da9306705729eb4176c0874335ef8d03
RedCommand <maxvidal1612@gmail.com>
2022-07-08 17:08:39 +0200

    [add] default theme component
    [add] default container for blocks
    
    [add] default navbar
    
    [add] color theme switcher component (not implemented yet
    
    [add] icon component as navbar ant color theme switcher depend on it

---
+++ components/Container.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps({
+  padded: {
+    type: Boolean,
+    required: false,
+    default: false
+  }
+})
+</script>
+
+<template>
+  <div
+    :class="[padded ? 'px-4 sm:px-6' : '']"
+    class="mx-auto  py-[36px]"
+  >
+    <slot />
+  </div>
+</template>
---
+++ components/Navbar.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+const { navigation } = useContent()
+</script>
+
+<template>
+  <div>
+    <div>
+      <!-- Title -->
+      <div class="text-3xl font-bold text-center text-gray-900 dark:text-gray-100">
+        <p>Alpine.</p>
+      </div>
+      <div class="absolute space-x-3 text-gray-500 transition right-10 top-10">
+        <ColorModeSwitch class="hover:text-gray-700 dark:hover:text-gray-300" />
+      </div>
+    </div>
+    <!-- Navigation -->
+    <div
+      class="flex justify-center max-w-2xl px-4 py-4 mx-auto ustify-between sm:px-8"
+    >
+      <div class="text-gray-700 dark:text-gray-200">
+        <NuxtLink
+          v-for="link of navigation"
+          :key="link._path"
+          :to="link._path"
+          active-class="font-bold"
+          class="mr-4"
+        >
+          {{ link.navTitle || link.title }}
+        </NuxtLink>
+      </div>
+      <!-- Social icons & Color Mode -->
+    </div>
+  </div>
+</template>
