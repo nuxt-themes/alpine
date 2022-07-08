fcd7c4f7fcad05069be1431ae4c5b552e3cfeb7c
RedCommand <maxvidal1612@gmail.com>
2022-07-08 17:07:01 +0200

    [add] default Alpine layout

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="text-gray-700 dark:text-gray-200">
+  <Navbar />
+    <main class="prose dark:prose-invert px-4 m-auto lg:px-0 max-w-[864px] break-words">
+      <slot />
+    </main>
+  </div>
+</template>
+
+<style lang="postcss">
+/* Customize headers to remove default underline */
+.prose p {
+  @apply my-[36px]
+}
+.prose h2 a,
+.prose h3 a {
+  @apply no-underline;
+  &:hover {
+    @apply border-b border-neutral-600 dark:border-neutral-300;
+  }
+}
+</style>
