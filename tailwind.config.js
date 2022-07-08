bc6169c10287bec5f4fc1d37c3037a2f477cbfd7
RedCommand <maxvidal1612@gmail.com>
2022-07-08 17:05:51 +0200

    [add] tailwind Alpine config

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  darkMode: 'class',
+  plugins: [
+    require('@tailwindcss/typography')
+  ],
+  content: [
+    'content/**/*.{md,yml,json,json5,csv}'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // Customize the feeling of your site
+        gray: colors.stone
+      }
+    }
+  }
+}
