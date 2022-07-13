b190da108e117d2057e683c0f014b83f786749e3
RedCommand <maxvidal1612@gmail.com>
2022-07-13 10:56:58 +0200

    fix(Hero): replace all pixel size with tailwind ones

--- components/content/Hero.vue
+++ components/content/Hero.vue
@@ -21,40 +21,33 @@ defineProps({
 
 <template>
   <section v-if="title || $slots.title" class="not-prose">
-    <Container :class="image ? 'grid lg:grid-flow-col-dense lg:justify-between' : ''">
-      <div :class="image ? 'lg:max-w-[420px] shrink' : ''">
+    <div :class="image ? 'lg:flex' : ''" class="">
+      <div :class="image ? 'shrink' : ''">
         <div class="text-3xl font-bold text-gray-900 dark:text-gray-100">
           <p v-if="title" class="!my-0">
             {{ title }}
           </p>
-          <Markdown
-            v-else
-            :use="$slots.title"
-            unwrap="p"
-          />
+          <Markdown v-else :use="$slots.title" unwrap="p" />
         </div>
-        <div
-          v-if="description || $slots.description"
-          class="mt-3 dark:prose-invert"
-        >
+        <div v-if="description || $slots.description" class="mt-3 dark:prose-invert">
           <p v-if="description" class="!my-0">
             {{ description }}
           </p>
-          <Markdown
-            v-else
-            :use="$slots.description"
-            unwrap="p"
-          />
+          <Markdown v-else :use="$slots.description" unwrap="p" />
         </div>
       </div>
       <img
         v-if="image"
-        class="max-h-[178px] object-cover w-full rounded-xl lg:mt-0 md:max-h-[274px] lg:h-[192px] lg:w-[388px]"
-        :class="layout === 'left' ? 'order-first lg:mr-[56px] mb-8' : 'lg:ml-[56px] mt-8'"
+        class="lg:flex-none max-h-44 object-cover w-full rounded-xl lg:mt-0 md:max-h-72 lg:h-48 lg:w-96"
+        :class="
+          layout === 'left'
+            ? 'order-first lg:mr-14 mb-8'
+            : 'lg:ml-14 mt-8'
+        "
         :src="image"
         alt=":("
       >
-    </Container>
+    </div>
   </section>
   <p v-else>
     :( a hero block must have a title !
