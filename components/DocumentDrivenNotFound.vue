2f2d786ed55c467b4de7661fa057976fb5c89946
RedCommand <maxvidal1612@gmail.com>
2022-07-13 17:34:36 +0200

    feat: add 404 page

---
+++ components/DocumentDrivenNotFound.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="absolute flex items-center justify-center left-0 top-0 w-screen h-screen text-center">
+    <!--  -->
+    <div>
+      <p class="text-5xl mb-5 mt-0 font-bold">
+        404
+      </p>
+      <p class="m-0">
+        This page could not be found.
+      </p>
+
+      <p class="m-0 mt-8">
+        <nuxt-link to="/">
+          Back to homepage
+        </nuxt-link>
+        <Icon name="uil:arrow-right" />
+      </p>
+    </div>
+  </div>
+</template>
