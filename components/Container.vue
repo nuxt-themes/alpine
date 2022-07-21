abbc82014815db7892911f4b573d930ed0ad6681
RedCommand <maxvidal1612@gmail.com>
2022-07-21 14:25:49 +0200

    fix: add better colors because palette() function is too light

--- components/Container.vue
+++ components/Container.vue
@@ -9,7 +9,7 @@ defineProps({
 
 <template>
   <div :class="useColorMode().value">
-    <div class="bg-secondary-50 dark:bg-secondary-900 h-full pb-0 mb-0">
+    <div class="bg-secondary-100 dark:bg-secondary-900 h-full pb-0 mb-0">
       <div :class="[padded ? 'px-4 sm:px-6' : '']" class="max-w-sm mx-auto md:max-w-2xl lg:max-w-4xl">
         <slot />
       </div>
--- tokens.config.ts
+++ tokens.config.ts
@@ -5,7 +5,31 @@ export default defineTokens({
     white: {
       value: '#FFFFFF'
     },
-    primary: palette('#FFF'),
-    secondary: palette('#FFF')
+    primary: {
+      DEFAULT: { value: '#78716c' },
+      50: { value: '#fafaf9' },
+      100: { value: '#f5f5f4' },
+      200: { value: '#e7e5e4' },
+      300: { value: '#d6d3d1' },
+      400: { value: '#a8a29e' },
+      500: { value: '' },
+      600: { value: '#57534e' },
+      700: { value: '#44403c' },
+      800: { value: '#292524' },
+      900: { value: '#1c1917' }
+    },
+    secondary: {
+      DEFAULT: { value: '#78716c' },
+      50: { value: '#fafaf9' },
+      100: { value: '#f5f5f4' }, // background light
+      200: { value: '#e7e5e4' },
+      300: { value: '#d6d3d1' },
+      400: { value: '#a8a29e' },
+      500: { value: '' },
+      600: { value: '#57534e' },
+      700: { value: '#44403c' },
+      800: { value: '#292524' },
+      900: { value: '#0c0c0d' } // background dark
+    }
   }
 })
