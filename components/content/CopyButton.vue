88a4c94c7322e8b98a1137e772d0f8ac618fcf9f
RedCommand <maxvidal1612@gmail.com>
2022-07-25 10:24:47 +0200

    feat: add <CodeBlock> component to prose

---
+++ components/content/CopyButton.vue
@@ -0,0 +1,38 @@
+<template>
+  <button class="flex flext-col absolute bottom-4 right-4 md:bottom-5 md:right-5 text-primary-100 not-prose my-0 bg-zinc-600 rounded-lg py-1 px-3" @click="copyCode">
+    <Icon v-if="state === 'copied'" name="uil:check" class="w-5 h-5 mr-1 mt-1" />
+    <p>
+      {{ state }}
+    </p>
+  </button>
+</template>
+
+<script setup lang="ts">
+import { useClipboard } from '@vueuse/core'
+
+const props = defineProps({
+  content: {
+    type: String,
+    default: ''
+  }
+})
+
+const state = ref('copy')
+const { copy: copyToClipboard } = useClipboard()
+
+const copyCode = () => {
+  copyToClipboard(props.content)
+    .then(() => {
+      state.value = 'copied'
+
+      setTimeout(() => {
+        state.value = 'copy'
+      }, 1500)
+    })
+    .catch(() => {
+      // eslint-disable-next-line no-console
+      console.warn("Couldn't copy to clipboard!")
+    })
+}
+
+</script>
---
+++ components/content/Prose/ProseCode.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import type { PropType } from 'vue'
+import type { Lang } from 'shiki-es'
+
+const props = defineProps({
+  code: {
+    type: String,
+    default: ''
+  },
+  language: {
+    type: String as PropType<Lang>,
+    default: null
+  },
+  filename: {
+    type: String,
+    default: null
+  },
+  highlights: {
+    type: Array as () => number[],
+    default: () => []
+  }
+})
+console.log('code: ', props.code)
+console.log('lang: ', props.language)
+console.log('filename: ', props.filename)
+</script>
+
+<template>
+  <div class="relative">
+    <CopyButton :content="code" />
+    <p v-if="filename" class="absolute transition hover:opacity-0 top-3 right-4 md:top-4 md:right-5 text-primary-100 not-prose my-0">
+      {{ filename }}
+    </p>
+    <div class="w-full prose-code prose-code:flex prose-code:flex-col">
+      <slot />
+    </div>
+  </div>
+</template>
