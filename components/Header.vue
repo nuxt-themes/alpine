d86ca039733b8fd76c270dea6553d012c68a84af
Florent <florentdelerue@hotmail.com>
2022-08-04 16:17:45 +0200

    refactor(header): add inline position and refacto code

--- components/Header.vue
+++ components/Header.vue
@@ -5,39 +5,52 @@ const theme = useTheme()
 const placeItems = computed(() => {
   switch (theme.value.header.position) {
     case 'left':
-      return 'place-items-start'
+      return 'items-start'
     case 'center':
-      return 'place-items-center'
+      return 'items-center'
     case 'right':
-      return 'place-items-end'
+      return 'items-end'
+    case 'inline':
+      return 'sm:flex-row justify-between'
     default:
-      return 'place-items-start'
+      return 'items-start'
   }
 })
 </script>
 
 <template>
-  <header class="relative">
-    <ColorModeSwitch class="absolute top-6 md:top-8" :class="{'right-0' : theme.header.position === 'left'}" />
+  <header class="relative h-32 md:h-36 py-8 mb-6 md:mb-8 flex items-center justify-between">
+    <ColorModeSwitch
+      :class="[
+        { 'absolute top-6 md:top-8': !(theme.header.position === 'inline') },
+        { 'order-last self-start md:self-auto': theme.header.position === 'inline' },
+        { 'right-0' : ['left', 'center'].includes(theme.header.position) }
+      ]"
+    />
     <div
-      class="grid h-32 grid-cols-1 grid-rows-2 py-6 mb-6 md:h-36 md:py-8 md:mb-8"
+      class="flex flex-col gap-y-[24px] md:gap-y-[32px] flex-1"
       :class="placeItems"
     >
       <NuxtLink v-if="theme.header.logo" to="/" class="text-primary-700 dark:text-primary-200">
         <img class="dark-img" :src="theme.header.logo.pathDark" :alt="theme.header.logo.alt">
         <img class="light-img" :src="theme.header.logo.path" :alt="theme.header.logo.alt">
       </NuxtLink>
-      <div class="grid grid-flow-col mt-8 gap-x-9 auto-cols-max">
-        <NuxtLink
+      <ul class="flex h-full sm:justify-center gap-x-[32px]" :class="{ 'flex-1': theme.header.position }">
+        <li
           v-for="link of navigation"
           :key="link._path"
-          :to="link._path"
-          active-class="font-bold"
-          class="text-primary-700 dark:text-primary-200"
+          class="relative group"
         >
-          {{ link.title }}
-        </NuxtLink>
-      </div>
+          <span class="absolute -bottom-1 w-0 h-0.5 bg-primary-700 group-hover:w-full transition-all duration-200 ease-in-out" />
+          <NuxtLink
+            :to="link._path"
+            active-class="font-bold"
+            class="text-primary-700 dark:text-primary-200"
+          >
+            {{ link.title }}
+          </NuxtLink>
+        </li>
+      </ul>
     </div>
   </header>
 </template>
