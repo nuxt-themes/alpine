d08fd6e70f550cf94ff67a345d7df50a596b5363
Florent <florentdelerue@hotmail.com>
2022-08-05 11:07:28 +0200

    refactor(header): up

--- components/Header.vue
+++ components/Header.vue
@@ -41,7 +41,8 @@ const placeItems = computed(() => {
           :key="link._path"
           class="relative group"
         >
-          <span class="absolute -bottom-1 w-0 h-0.5 bg-primary-700 group-hover:w-full transition-all duration-200 ease-in-out" />
+          <!-- underline -->
+          <span class="absolute -bottom-1 w-0 h-0.5 bg-primary-700 dark:bg-primary-100 group-hover:w-full transition-all duration-200 ease-in-out" />
           <NuxtLink
             :to="link._path"
             active-class="font-bold"
