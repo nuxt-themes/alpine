152f32afc4ee474a5830ff9f07740b44e53c2456
Sébastien Chopin <seb@nuxtjs.com>
2022-08-16 17:01:07 +0200

    chore: add link to default config

--- template/theme.config.ts
+++ template/theme.config.ts
@@ -1,5 +1,6 @@
 import { defineTheme } from '@nuxt-themes/config'
 
+// Default config: https://github.com/nuxt-themes/alpine/blob/dev/theme/theme.config.ts
 export default defineTheme({
   // name: 'My project'
 })
