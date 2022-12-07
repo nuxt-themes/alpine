e8705a7b6979a385619a974c10c500d5a583104b
Serhii Bedrytskyi <bedrytskyi@gmail.com>
2022-12-07 16:50:52 +0200

    feat: add mobile menu (#83)
    Co-authored-by: Yaël Guilloux <yael.guilloux@gmail.com>

--- tokens.config.ts
+++ tokens.config.ts
@@ -25,32 +25,6 @@ export default defineTheme({
     white: {
       value: '#FFFFFF'
     },
-    primary: theme.color.yellow,
-    // primary: {
-    //   DEFAULT: { value: '#78716c' },
-    //   50: { value: '#ffffff' },
-    //   100: { value: '#ffffff' },
-    //   200: { value: '#e7e5e4' },
-    //   300: { value: '#d6d3d1' },
-    //   400: { value: '#a8a29e' },
-    //   500: { value: '' },
-    //   600: { value: '#57534e' },
-    //   700: { value: '#44403c' },
-    //   800: { value: '#292524' },
-    //   900: { value: '#1c1917' }
-    // },
-    secondary: {
-      DEFAULT: { value: '#78716c' },
-      50: { value: '#fafaf9' },
-      100: { value: '#f5f5f4' }, // background light
-      200: { value: '#e7e5e4' },
-      300: { value: '#d6d3d1' },
-      400: { value: '#a8a29e' },
-      500: { value: '' },
-      600: { value: '#57534e' },
-      700: { value: '#44403c' },
-      800: { value: '#292524' },
-      900: { value: '#0c0c0d' } // background dark
-    }
+    primary: theme.color.yellow
   }
 })
