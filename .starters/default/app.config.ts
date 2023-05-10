c4339ce2c534bf57e9bb05cb1ba5cfbc0e6b1d3b
Yaël Guilloux <yael.guilloux@gmail.com>
2023-05-10 17:20:26 +0200

    feat(.starters): import alpine-starter into `.starters/default`

---
+++ .starters/default/app.config.ts
@@ -0,0 +1,41 @@
+export default defineAppConfig({
+  alpine: {
+    title: 'Alpine',
+    description: 'The minimalist blog theme',
+    image: {
+      src: '/social-card-preview.png',
+      alt: 'An image showcasing my project.',
+      width: 400,
+      height: 300
+    },
+    header: {
+      position: 'right', // possible value are : | 'left' | 'center' | 'right'
+      logo: {
+        path: '/logo.svg', // path of the logo
+        pathDark: '/logo-dark.svg', // path of the logo in dark mode, leave this empty if you want to use the same logo
+        alt: 'alpine' // alt of the logo
+      }
+    },
+    footer: {
+      credits: {
+        enabled: true, // possible value are : true | false
+        repository: 'https://www.github.com/nuxt-themes/alpine' // our github repository
+      },
+      navigation: true, // possible value are : true | false
+      alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
+      message: 'Follow me on' // string that will be displayed in the footer (leave empty or delete to disable)
+    },
+    socials: {
+      twitter: 'nuxtlabs',
+      instagram: 'atinuxt',
+      linkedin: {
+        icon: 'uil:linkedin',
+        label: 'LinkedIn',
+        href: 'https://www.linkedin.com/company/nuxtlabs'
+      }
+    },
+    form: {
+      successMessage: 'Message sent. Thank you!'
+    }
+  }
+})
