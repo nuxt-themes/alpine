1d6d2661adda581e7f1a6da32be7735787fee0a0
Анна Серба <a.serba@superjob.ru>
2024-01-22 20:22:04 +0300

    добаление проекта

--- .starters/default/app.config.ts
+++ .starters/default/app.config.ts
@@ -1,41 +1,50 @@
 export default defineAppConfig({
   alpine: {
-    title: 'Alpine',
-    description: 'The minimalist blog theme',
+    title: 'Счастье в путешествиях',
+    description: 'Я делюсь своим опытом и знаниями, чтобы помочь вам открыть для себя мир и сделать ваше путешествие ярким и запоминающимся.',
+    keywords: 'Путешествие, Азия, Тайланд, Владивосток, Китай, Вьетнам',
     image: {
-      src: '/social-card-preview.png',
-      alt: 'An image showcasing my project.',
+      src: '/aonang.jpg',
+      alt: 'Анна путешественица',
       width: 400,
       height: 300
     },
+
     header: {
       position: 'right', // possible value are : | 'left' | 'center' | 'right'
       logo: {
         path: '/logo.svg', // path of the logo
-        pathDark: '/logo-dark.svg', // path of the logo in dark mode, leave this empty if you want to use the same logo
-        alt: 'alpine' // alt of the logo
+        pathDark: '/logo.svg', // path of the logo in dark mode, leave this empty if you want to use the same logo
+        alt: 'Путешествие' // alt of the logo
       }
     },
+
     footer: {
       credits: {
-        enabled: true, // possible value are : true | false
+        enabled: false, // possible value are : true | false
         repository: 'https://www.github.com/nuxt-themes/alpine' // our github repository
       },
       navigation: true, // possible value are : true | false
       alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
-      message: 'Follow me on' // string that will be displayed in the footer (leave empty or delete to disable)
+      message: '' // string that will be displayed in the footer (leave empty or delete to disable)
     },
+
     socials: {
-      twitter: 'nuxtlabs',
-      instagram: 'atinuxt',
+      twitter: '',
+      instagram: '',
       linkedin: {
-        icon: 'uil:linkedin',
-        label: 'LinkedIn',
-        href: 'https://www.linkedin.com/company/nuxtlabs'
+        icon: 'uil:vk',
+        label: 'VK',
+        href: 'https://vk.com/happinessintravel'
       }
     },
+
     form: {
       successMessage: 'Message sent. Thank you!'
+    },
+
+    backToTop: {
+      text: 'К началу'
     }
   }
 })
