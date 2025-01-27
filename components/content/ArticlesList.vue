2b210d88da5f4fab25520486cb26b6f8b2d51c40
4xeverburga <ever.burga.p@uni.pe>
2025-01-27 01:57:58 -0500

    added: nav buttons to articles. Not finished

--- components/content/ArticlesList.vue
+++ components/content/ArticlesList.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { withTrailingSlash } from 'ufo'
+import { ref, computed } from 'vue'
 
 const props = defineProps({
   path: {
@@ -12,24 +13,52 @@ const props = defineProps({
 const { data: _articles } = await useAsyncData(props.path, async () => await queryContent(withTrailingSlash(props.path)).sort({ date: -1 }).find())
 
 const articles = computed(() => _articles.value || [])
+
+// Get the unique years from the articles
+const years = computed(() => {
+  const yearsSet = new Set(articles.value.map(article => new Date(article.date).getFullYear()))
+  return Array.from(yearsSet).sort((a, b) => b - a)
+})
+
+const currentYear = ref(years.value[0])
+
+const filteredArticles = computed(() => {
+  return articles.value.filter(article => new Date(article.date).getFullYear() === currentYear.value)
+})
+
+const showNextYear = () => {
+  const currentIndex = years.value.indexOf(currentYear.value)
+  if (currentIndex > 0) {
+    currentYear.value = years.value[currentIndex - 1]
+  }
+}
+
+const showPreviousYear = () => {
+  const currentIndex = years.value.indexOf(currentYear.value)
+  if (currentIndex < years.value.length - 1) {
+    currentYear.value = years.value[currentIndex + 1]
+  }
+}
 </script>
 
 <template>
-  <div v-if="articles?.length" class="articles-list">
-    <div class="featured">
-      <ArticlesListItem :article="articles[0]" :featured="true" />
+  <div>
+    <div v-if="filteredArticles?.length" class="articles-list">
+      <div class="featured">
+        <ArticlesListItem :article="filteredArticles[0]" :featured="true" />
+      </div>
+      <div class="layout">
+        <ArticlesListItem v-for="(article, index) in filteredArticles.slice(1)" :key="index" :article="article" />
+      </div>
     </div>
-    <div class="layout">
-      <ArticlesListItem v-for="(article, index) in articles.slice(1)" :key="index" :article="article" />
+    <div v-else class="tour">
+      <p>Seems like there are no articles for {{ currentYear }}.</p>
     </div>
-  </div>
-  <div v-else class="tour">
-    <p>Seems like there are no articles yet.</p>
-    <p>
-      You can start by
-      <!-- eslint-disable-next-line -->
-      <ProseA href="https://alpine.nuxt.space/articles/write-articles">creating</ProseA> one in the <ProseCodeInline>articles</ProseCodeInline> folder.
-    </p>
+
+    <div class="navigation-buttons">
+      <button @click="showPreviousYear" :disabled="currentYear === years[years.length - 1]">Previous Year</button>
+      <button @click="showNextYear" :disabled="currentYear === years[0]">Next Year</button>
+    </div>
   </div>
 </template>
 
@@ -67,6 +96,20 @@ css({
     flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  '.navigation-buttons': {
+    display: 'flex',
+    justifyContent: 'space-between',
+    marginTop: '20px',
+    button: {
+      padding: '10px 20px',
+      fontSize: '16px',
+      cursor: 'pointer',
+      '&:disabled': {
+        cursor: 'not-allowed',
+        opacity: 0.5,
+      }
+    }
   }
 })
 </style>
